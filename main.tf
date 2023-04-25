5d7ceb9ef1c3720195709d353e731a1e27534bfa
Ryan Faircloth <ryan@dss-i.com>
2023-04-25 14:17:04 -0400

    feat: add pools

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "service_accounts" {
 }
 
 module "gke" {
-  source  = "terraform-google-modules/kubernetes-engine/google"
+  source = "terraform-google-modules/kubernetes-engine/google"
   # version = "25.0.0"
   release_channel = "RAPID"
 
@@ -65,6 +65,101 @@ module "gke" {
     "min_cpu_cores" : 12,
     "min_memory_gb" : 12
   }
+
+  node_pools = [
+    {
+      name            = "general"
+      min_count       = 0
+      max_count       = 5
+      service_account = var.compute_engine_service_account
+      auto_upgrade    = true
+      auto_repair     = true
+    },
+    {
+      name         = "compute"
+      # machine_type = "a2-highgpu-1g"
+      min_count    = 0
+      max_count    = 5
+      # local_ssd_count    = 0
+      # disk_size_gb       = 30
+      # disk_type          = "pd-standard"
+      # accelerator_count  = 1
+      # accelerator_type   = "nvidia-tesla-a100"
+      # gpu_partition_size = "1g.5gb"
+      auto_repair     = true
+      service_account = module.service_accounts.email
+    }
+    # {
+    #   name               = "pool-03"
+    #   machine_type       = "n1-standard-2"
+    #   node_locations     = "${var.region}-b,${var.region}-c"
+    #   autoscaling        = false
+    #   node_count         = 2
+    #   disk_type          = "pd-standard"
+    #   auto_upgrade       = true
+    #   service_account    = var.compute_engine_service_account
+    #   pod_range          = "test"
+    #   sandbox_enabled    = true
+    #   cpu_manager_policy = "static"
+    #   cpu_cfs_quota      = true
+    # },
+  ]
+
+  # node_pools_metadata = {
+  #   pool-01 = {
+  #     shutdown-script = "kubectl --kubeconfig=/var/lib/kubelet/kubeconfig drain --force=true --ignore-daemonsets=true --delete-local-data \"$HOSTNAME\""
+  #   }
+  # }
+
+  node_pools_labels = {
+    # all = {
+    #   all-pools-example = true
+    # }
+    general = {
+      workloadClass = "general"
+    }
+    compute = {
+      workloadClass = "compute"
+    }
+  }
+
+  # node_pools_taints = {
+  #   all = [
+  #     {
+  #       key    = "all-pools-example"
+  #       value  = true
+  #       effect = "PREFER_NO_SCHEDULE"
+  #     },
+  #   ]
+  #   pool-01 = [
+  #     {
+  #       key    = "pool-01-example"
+  #       value  = true
+  #       effect = "PREFER_NO_SCHEDULE"
+  #     },
+  #   ]
+  # }
+
+  # node_pools_tags = {
+  #   all = [
+  #     "all-node-example",
+  #   ]
+  #   pool-01 = [
+  #     "pool-01-example",
+  #   ]
+  # }
+
+  # node_pools_linux_node_configs_sysctls = {
+  #   all = {
+  #     "net.core.netdev_max_backlog" = "10000"
+  #   }
+  #   pool-01 = {
+  #     "net.core.rmem_max" = "10000"
+  #   }
+  #   pool-03 = {
+  #     "net.core.netdev_max_backlog" = "20000"
+  #   }
+  # }
 }
 
 module "gke_auth" {
