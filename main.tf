a092c885dc5434f12d3fa6abf0c07dcc0d5d0a37
Ryan Faircloth <ryan@dss-i.com>
2023-04-27 09:45:37 -0400

    fix: add pools

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ module "gke" {
       name         = "general"
       machine_type = "e2-standard-4"
       min_count    = 3
-      max_count    = 15
+      max_count    = 6
       # service_account = format("%s@%s.iam.gserviceaccount.com", local.cluster_sa_name, var.project_id)
       auto_upgrade = true
       auto_repair  = true
@@ -83,7 +83,7 @@ module "gke" {
       name         = "compute"
       machine_type = "e2-highcpu-16"
       min_count    = 3
-      max_count    = 15
+      max_count    = 6
       # local_ssd_count    = 0
       # disk_size_gb       = 30
       # disk_type          = "pd-standard"
