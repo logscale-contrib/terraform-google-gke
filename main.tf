f20be90160f463cadcb706a6b3d7d97d0d8bb10c
Ryan Faircloth <ryan@dss-i.com>
2023-04-27 16:40:34 -0400

    fix: add pools

--- main.tf
+++ main.tf
@@ -81,9 +81,9 @@ module "gke" {
     },
     {
       name         = "compute"
-      machine_type = "c2-standard-16"
+      machine_type = "c2-standard-8"
       min_count    = 1
-      max_count    = 1
+      max_count    = 2
       # local_ssd_count    = 0
       # disk_size_gb       = 30
       # disk_type          = "pd-standard"
